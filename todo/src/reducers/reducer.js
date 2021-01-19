87092bf550a57d56e50ab6864de2e0c47368dd5b
Mark <mark@Marks-Air.home>
2021-01-19 15:22:39 -0500

    fixed reducer/state

--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,14 +1,24 @@
-let currentState = reducer({
-    item: 'Learn about reducers',
-    completed: false,
-    id: 3892987589
-  })
+import React, { useState, useReducer } from 'react'
 
-const reducer = (state, action) => {
-    switch (action.type) {
-        default:
-            return state
+const Reducer = () => {
+    const [state, setState] = useState({
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+      })
+
+    const handleChanges = e => {
+        setState(e.target.value)
     }
+
+
+    return (
+        <div>
+            <h1>{state.item}</h1>
+            <h2>{state.completed}</h2>
+            <h3>{state.id}</h3>
+        </div>
+    )
 }
 
-export default reducer
+export default Reducer
