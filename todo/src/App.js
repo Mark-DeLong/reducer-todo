dd0bb50595719786efe2962acb7a8f0982706f12
Mark <mark@Marks-Air.home>
2021-01-19 19:57:16 -0500

    but wait theres more

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,17 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react'
-import Form from './components/Form'
+import React, { useReducer, useState } from 'react'
+import './App.css'
 
-function App() {
-  
+import { initialState, reducer} from './reducers/reducer'
+
+const App = () => {
+  const [task, setTask] = useState('')
+
+  const [state, dispatch] =  useReducer(reducer, initialState)
+
+  const handlechange = e => {
+    setTask(e.target.value)
+  }
+
+  const onSubmit = e => {
+      e.preventDefault()
+
+      dispatch({type:"ADD_TODO", payload:task})
+      setTask('')
+  }
+
+  const toggleTask = (id) =>{
+    dispatch({type:"TASK_TOGGLED", payload: id})
+  }
+
+  const clearCompleted = e => {
+    e.preventDefault()
+
+    dispatch({type:"CLEAR_COMPLETED",})
+  }
+
   return (
-    <div>
-      <h1>To Do List</h1>
-      <Form />
+    <div 
+      className="App"
+    >
+      <h1>
+        Todo List
+      </h1>
+      <form>
+        <input
+          type="text"
+          value={task}
+          name='item'
+          onChange={handlechange}
+        />
+        <button 
+          onClick={onSubmit}
+        >
+          Add
+        </button>
+      </form>
+      <div>
+            {state.map(todo => (
+              <div
+                className={`${todo.completed ? 'completed' : ''}`}
+                onClick={() => toggleTask(todo.id)}
+              >
+                  <p>
+                    {todo.item}
+                  </p>
+              </div>
+            ))}
+            <button 
+              className="clearBtn" 
+              onClick={clearCompleted}
+            >
+              Clear
+            </button>
+        </div>
     </div>
   )
-   
 }
 
-export default App;
+export default App
